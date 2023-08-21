326b3a7304c11e49240c228b055bdac80fb4aa53
Omer <of.c.58@hotmail.com>
2023-08-21 12:22:39 +0200

    Home Screen layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,13 +7,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Start Game"),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, GameScreen.routeName);
-          },
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.red,
+            child: const Padding(
+              padding: EdgeInsets.only(top:100.0), // Adjust the top padding as needed
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  "Three-in-a-Row", // Your custom title
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: const Text("Start Game"),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, GameScreen.routeName);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
